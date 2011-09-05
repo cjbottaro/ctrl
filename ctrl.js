8d75f626b4dd3aa990c8b1ea324ad4df34691653
Christopher J. Bottaro <cjbottaro@alumni.cs.utexas.edu>
2011-09-05 03:53:18 -0400

    Generated JS from CS.

--- ctrl.js
+++ ctrl.js
@@ -1,13 +1,68 @@
 (function() {
-  var Ctrl, myTimeout;
-  var __slice = Array.prototype.slice, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-  myTimeout = function(msg, time, callback) {
-    return setTimeout(function() {
-      return callback(msg, time);
-    }, time);
-  };
+  var Ctrl;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __slice = Array.prototype.slice;
   Ctrl = (function() {
-    function Ctrl() {}
+    function Ctrl() {
+      this.execNextStep = __bind(this.execNextStep, this);
+    }
+    Ctrl.Step = (function() {
+      function Step(func, cont) {
+        this.func = func;
+        this.cont = cont;
+        this.async_started = 0;
+        this.async_finished = 0;
+        this.a_results = [];
+        this.h_results = {};
+      }
+      Step.prototype.execute = function(ctrl) {
+        this.func(ctrl);
+        if (this.async_started === 0) {
+          return this.finished();
+        }
+      };
+      Step.prototype.collect = function() {
+        var index, names;
+        names = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+        index = this.async_started;
+        this.async_started += 1;
+        return __bind(function() {
+          var name, result, results, _i, _len, _ref, _ref2;
+          results = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+          if (names.length > 0) {
+            _ref = Ctrl.zip(names, results);
+            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+              _ref2 = _ref[_i], name = _ref2[0], result = _ref2[1];
+              this.h_results[name] = result;
+            }
+          } else {
+            this.a_results[index] = results;
+          }
+          this.async_finished += 1;
+          if (this.async_started === this.async_finished) {
+            return this.finished();
+          }
+        }, this);
+      };
+      Step.prototype.finished = function() {
+        var result, _i, _len, _ref;
+        this.results = [];
+        _ref = this.a_results;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          result = _ref[_i];
+          if (result != null) {
+            if (result.length === 1) {
+              this.results.push(result[0]);
+            } else {
+              this.results.push(result);
+            }
+          }
+        }
+        this.named_results = this.h_results;
+        this.result = this.results[0];
+        return this.cont();
+      };
+      return Step;
+    })();
     Ctrl["new"] = function() {
       var ctrl, steps;
       steps = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
@@ -17,101 +72,7 @@
       }
       return ctrl;
     };
-    Ctrl.prototype.exec = function() {
-      var steps;
-      steps = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-      this.steps = steps;
-      this.index = 0;
-      this.i_results = [];
-      this.n_results = [];
-      return this.run_steps();
-    };
-    Ctrl.prototype.run_steps = function() {
-      var step, _results;
-      _results = [];
-      while (true) {
-        step = this.steps[this.index];
-        if (step == null) {
-          break;
-        }
-        this.call(step);
-        if (this.defers > 0) {
-          break;
-        }
-        _results.push(this.index += 1);
-      }
-      return _results;
-    };
-    Ctrl.prototype.call = function(step) {
-      this.defers = 0;
-      this.calls = 0;
-      this.i_results[this.index] = [];
-      this.n_results[this.index] = {};
-      this.expose_previous_results();
-      return step(this);
-    };
-    Ctrl.prototype.expose_previous_results = function() {
-      var result, results;
-      if (this.index === 0) {
-        this.result = null;
-        this.results = [];
-        this.named_results = {};
-        return;
-      }
-      results = (function() {
-        var _i, _len, _ref, _results;
-        _ref = this.i_results[this.index - 1];
-        _results = [];
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          result = _ref[_i];
-          if (result != null) {
-            _results.push(result);
-          }
-        }
-        return _results;
-      }).call(this);
-      if (results != null) {
-        this.result = results[0];
-        if (this.result.length === 1) {
-          this.result = this.result[0];
-        }
-        this.results = results;
-      } else {
-        this.result = null;
-        this.results = [];
-      }
-      results = this.n_results[this.index - 1];
-      if (results != null) {
-        return this.named_results = results;
-      } else {
-        return this.named_results = {};
-      }
-    };
-    Ctrl.prototype.collect = function() {
-      var index, names;
-      names = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-      index = this.defers;
-      this.defers += 1;
-      return __bind(function() {
-        var name, result, results, _i, _len, _ref, _ref2;
-        results = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-        if (names.length > 0) {
-          _ref = this.zip(names, results);
-          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-            _ref2 = _ref[_i], name = _ref2[0], result = _ref2[1];
-            this.n_results[this.index][name] = result;
-          }
-        } else {
-          this.i_results[this.index][index] = results;
-        }
-        this.calls += 1;
-        if (this.calls === this.defers) {
-          this.index += 1;
-          return this.run_steps();
-        }
-      }, this);
-    };
-    Ctrl.prototype.zip = function(ar1, ar2) {
+    Ctrl.zip = function(ar1, ar2) {
       var i, zipped;
       zipped = [];
       i = 0;
@@ -124,6 +85,50 @@
         zipped[i] = [ar1[i], ar2.slice(i, ar2.length)];
       }
       return zipped;
+    };
+    Ctrl.prototype.exec = function() {
+      var func, funcs;
+      funcs = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      this.steps = (function() {
+        var _i, _len, _results;
+        _results = [];
+        for (_i = 0, _len = funcs.length; _i < _len; _i++) {
+          func = funcs[_i];
+          _results.push(new Ctrl.Step(func, this.execNextStep));
+        }
+        return _results;
+      }).call(this);
+      this.index = -1;
+      return this.execNextStep();
+    };
+    Ctrl.prototype.currentStep = function() {
+      return this.steps[this.index];
+    };
+    Ctrl.prototype.previousStep = function() {
+      return this.steps[this.index - 1];
+    };
+    Ctrl.prototype.execNextStep = function() {
+      var current_step, previous_step;
+      this.index += 1;
+      previous_step = this.previousStep();
+      current_step = this.currentStep();
+      if (previous_step != null) {
+        this.result = previous_step.result;
+        this.results = previous_step.results;
+        this.named_results = previous_step.named_results;
+      } else {
+        this.result = null;
+        this.results = [];
+        this.named_results = {};
+      }
+      if (current_step != null) {
+        return current_step.execute(this);
+      }
+    };
+    Ctrl.prototype.collect = function() {
+      var names, _ref;
+      names = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      return (_ref = this.currentStep()).collect.apply(_ref, names);
     };
     return Ctrl;
   })();
